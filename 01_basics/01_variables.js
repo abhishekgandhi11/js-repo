9ef5507e74301a16031ae8c55cf9c7b698974430
Abhishek Gandhi <30987706+abhishekgandhi11@users.noreply.github.com>
2025-07-05 11:08:52 +0000

    basics

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 144
+let accountEmail = "ab@gmail.com"
+let accountPassword  = "12345"
+accountCity = "Navsari"
+let accountState;
+
+// accountId = 2
+accountEmail = "ag@gmail.com"
+accountPassword = "hhhhh"
+accountCity = "surat"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
