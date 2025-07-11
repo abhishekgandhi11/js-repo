88d5bb9e6b72fe9e7b2badffa0379d8e399a0722
Abhishek Gandhi <30987706+abhishekgandhi11@users.noreply.github.com>
2025-07-11 18:54:53 +0000

    data types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; //treat all js code as newer version
+
+//alert("hello") //we are using node js not browser
+
+console.log(3+3)
+console.log("Hello")
+
+let name = "Abhi"
+let age = 29
+let isLoggedIn = false
+
+//number
+//bigint
+//string
+//boolean
+//null
+//undefined
+// symbol
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
