47581c8416ceacec9699b1d19a95972f99dee561
Abhishek Gandhi <30987706+abhishekgandhi11@users.noreply.github.com>
2025-07-11 19:14:50 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,28 @@
+let score = "ABhi"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+let isLoggedIn = "abc"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+
+console.log(booleanIsLoggedIn);
+
+// 1 => true
+//  "" => false
+// "ab" => true
+
+let someNumber = 33
+let strinNumber = String(someNumber);
+
+console.log(strinNumber);
+console.log(typeof strinNumber);
